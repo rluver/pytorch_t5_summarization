6dc9e58e46debdd4f34707ad9bc11a33603dd2d6
rluver <skyd0608@gmail.com>
2021-07-28 13:17:10 +0900

    chore: Edit inference.py to change code
    change:
     - Move summarize to auxiliary.py
    
    fix:
     - change tokenizer from base to small

--- inference.py
+++ inference.py
@@ -1,48 +1,20 @@
 from model import Summarizer
-from auxiliary import load_data
+from auxiliary import Summarize
 from transformers import T5TokenizerFast as T5Tokenizer
 
 import torch
 
 
 
-def summarize(text):
-    
-    text_encoding = tokenizer.encode_plus(
-        text,
-        max_length = 512,
-        padding = 'max_length',
-        truncation = True,
-        return_attention_mask = True,
-        add_special_tokens = True,
-        return_tensors = 'pt'
-        )
-    
-    generated_ids = summarize_model.model.generate(
-        input_ids = text_encoding.input_ids,
-        attention_mask = text_encoding.attention_mask,
-        max_length = 196,
-        num_beams = 8,
-        repetition_penalty = 2.5,
-        length_penalty = 2.0,
-        early_stopping = True
-        )
-    
-    predicted_text = [
-        tokenizer.decode(generation_id, skip_special_tokens = True, clean_up_tokenization_spaces = True) for generation_id in generated_ids
-        ]
-    
-    return ''.join(predicted_text)
-
-
-
 
 if  __name__ == '__main__':
     
-    tokenizer = T5Tokenizer.from_pretrained('google/mt5-base')
+    tokenizer = T5Tokenizer.from_pretrained('google/mt5-small')
     
     summarize_model = Summarizer.load_from_checkpoint(r'checkpoints\best-checkpoint.ckpt')
     summarize_model.freeze()
     
-    print('input text: ')
-    summarize(input())
+    summarize = Summarize(tokenizer, summarize_model)
+    
+    text = ''
+    summarize.summarize(text)    
