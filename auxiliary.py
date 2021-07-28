2d4b39c90b04cfadc99a1b7f106ce580ad7d9ad2
rluver <skyd0608@gmail.com>
2021-07-28 13:14:32 +0900

    refactor: Edit auxiliary.py to chagne code
    refactor:
      - Make Summarize class

--- auxiliary.py
+++ auxiliary.py
@@ -21,3 +21,41 @@ def load_data(path, sep = '\t'):
     dataframe.dropna(inplace = True)
     
     return dataframe
+
+
+
+
+class Summarize:
+    
+    def __init__(self, tokenizer, summarize_model):
+        self.tokenizer = tokenizer
+        self.summarize_model = summarize_model
+
+
+    def summarize(self, text):
+        
+        text_encoding = self.tokenizer.encode_plus(
+            text,
+            max_length = 512,
+            padding = 'max_length',
+            truncation = True,
+            return_attention_mask = True,
+            add_special_tokens = True,
+            return_tensors = 'pt'
+            )
+        
+        generated_ids = self.summarize_model.model.generate(
+            input_ids = text_encoding.input_ids,
+            attention_mask = text_encoding.attention_mask,
+            max_length = 192,
+            num_beams = 8,
+            repetition_penalty = 2.5,
+            length_penalty = 2.0,
+            early_stopping = True
+            )
+        
+        predicted_text = [
+            self.tokenizer.decode(generation_id, skip_special_tokens = True, clean_up_tokenization_spaces = True) for generation_id in generated_ids
+            ]
+        
+        return ''.join(predicted_text)
